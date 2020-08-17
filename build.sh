f58b3945852a71aa59cdc34901ece6a7aa2ef39e
Marcos Vasconcelos <marvinred@gmail.com>
2020-08-17 00:19:23 -0300

    Ajuste commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+##
+####
+cd elk
+sudo docker-compose up --no-start
+cd ../
+cd jupyer-notebook
+sudo docker-compose build
+sudo docker run -d -p 8888:8888 --name pyspark-es --network es-net  jupyter-notebook_pyspark:latest

718691bfbca6210a1668dfb1a716310ac88bd504
Marcos Vasconcelos <marvinred@gmail.com>
2020-08-18 22:57:04 -0300

    Ajustes na inicialização dos containers

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 ##
 ####
 cd elk
-sudo docker-compose up
+sudo docker-compose up --no-start
 cd ../jupyter-notebook
-sudo docker-compose build
+sudo docker-compose build 
 sudo docker run -d -p 8888:8888 --name pyspark-es --network es-net  jupyter-notebook_pyspark:latest
+sudo docker container start grafana
+sudo docker container start kibana
+sudo docker container start elastic-node1

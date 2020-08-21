eeaa07b57e9b5dc07c29036725e8ae95070abbf6
Marcos Vasconcelos <marvinred@gmail.com>
2020-08-21 11:35:31 -0300

    Ajuste de senha do jupyter-notebook

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ sudo docker network create es-net -d "bridge"
 sudo docker-compose up --no-start
 cd ../jupyter-notebook
 sudo docker-compose build 
-sudo docker run -d -p 8888:8888 --name pyspark-es --network es-net  jupyter-notebook_pyspark:latest
+sudo docker run -d -p 8888:8888 -e JUPYTER_TOKEN="jpt@teste" --name pyspark-es --network es-net  jupyter-notebook_pyspark:latest
 sudo docker container start grafana
 sudo docker container start kibana
 sudo docker container start elastic-node1
